456880f6c860edb1c1c9fe10718dd601fbb8bc22
unknown <J.Domeikis@gmail.com>
2021-03-14 22:10:35 +0200

    First

--- HangMan.js
+++ HangMan.js
@@ -26,42 +26,75 @@ function randomWord() {
 }
 
 function generateButtons() {
-    let buttonsHTML = "abcdefghijklmnopqrstuvwxyz".split('').map(letter =>
+    let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
         `
-    <button
-     class="btn btn-lg btn-success m-2"
-      id='\` + letter + \`' onClick="handleGuess('\` + letter + \`')">` + letter + `</button>
-    `).join('')
-    document.getElementById('keyboard').innerHTML = buttonsHTML
+      <button
+        class="btn btn-lg btn-primary m-2"
+        id='` + letter + `'
+        onClick="handleGuess('` + letter + `')"
+      >
+        ` + letter + `
+      </button>
+    `).join('');
+
+    document.getElementById('keyboard').innerHTML = buttonsHTML;
+
+
 }
 
 
 
-// function guessedWord() {
-//     wordStatus = answer.split('').map(letter => guessed.indexOf(letter) >= 0 ? letter : " _ ").join('')
-//
-//     document.getElementById('wordSpotlight').innerHTML = wordStatus
-// }
-//
-// document.getElementById('maxWrong').innerHTML = maxWrong
-//
-// function handleGuess(chosenLetter) {
-//     guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null
-//     document.getElementById(chosenLetter).setAttribute('disabled', true)
-//
-//     console.log('aaaaaa')
-//
-//     if(answer.indexOf(chosenLetter) >= 0) {
-//         guessedWord()
-//     }
-// }
+
+
+
+function handleGuess(chosenLetter) {
+    guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null
+    document.getElementById(chosenLetter).setAttribute('disabled', true)
+
+    alert(answer)
+    if(answer.indexOf(chosenLetter) >= 0) {
+        guessedWord()
+    } else if (answer.indexOf(chosenLetter) === -1) {
+        mistakes++
+        updateMistakes()
+    }
+}
+
+
+
+
+
+
+
+
+
+function guessedWord() {
+    wordStatus = answer.split('').map(letter => guessed.indexOf(letter) >= 0 ? letter : " _ ").join('')
+
+    document.getElementById('wordSpotlight').innerHTML = wordStatus
+}
+
+function updateMistakes() {
+    document.getElementById('mistakes').innerHTML = mistakes
+}
+
+
+document.getElementById('maxWrong').innerHTML = maxWrong
+
+
+
+
+
+
+
+
 
 
 
 randomWord()
 generateButtons()
-// guessedWord()
-// handleGuess()
+guessedWord()
+handleGuess()
 
 
 
